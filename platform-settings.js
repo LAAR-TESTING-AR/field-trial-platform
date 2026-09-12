b0d244e3b0a9184e9ac12a88f46d6a29d505b987
sebamirandamiranda-ship-it <sebamiranda.miranda@gmail.com>
2026-09-12 13:15:30 -0300

    Update platform-settings.js

--- platform-settings.js
+++ platform-settings.js
@@ -1,10 +1,17 @@
 window.PlatformSettings = {
 
   isEnabled(settingName) {
+    return localStorage.getItem(settingName) === "true";
+  },
 
-    return localStorage.getItem(settingName)
-      === "true";
-
+  getAll() {
+    return {
+      trialPhotos: this.isEnabled("trialPhotos"),
+      visitComments: this.isEnabled("visitComments"),
+      visitScore: this.isEnabled("visitScore"),
+      cropStage: this.isEnabled("cropStage"),
+      timeline: this.isEnabled("timeline")
+    };
   }
 
 };
