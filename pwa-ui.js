67e6452d12c0d7cb63758834ab069221d90535e7
sebamirandamiranda-ship-it <sebamiranda.miranda@gmail.com>
2026-09-14 15:08:52 -0300

    Update pwa-ui.js

--- pwa-ui.js
+++ pwa-ui.js
@@ -35,13 +35,13 @@
       ${
   window.location.pathname.includes("index.html")
   ? `
-  <button
-    id="syncMasterList"
-    class="pwa-boton-sync"
-    type="button">
-    🔄 Sync
-  </button>
-  `
+ <button
+  id="syncMasterList"
+  class="master-sync-icon"
+  type="button"
+  title="Actualizar datos">
+  🔄
+</button>
   : ""
 }
         <span
