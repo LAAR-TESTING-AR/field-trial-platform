49a7d51ee3984382dce1a54e20c47e05a7c88f84
sebamirandamiranda-ship-it <sebamiranda.miranda@gmail.com>
2026-09-15 16:01:38 -0300

    Update platform-features.js

--- platform-features.js
+++ platform-features.js
@@ -1,13 +1,27 @@
 window.FIELD_TRIAL_FEATURES = {
-
- VISITS:
-  localStorage.getItem("trialVisits") === "true",
-
-
+  VISITS: localStorage.getItem("trialVisits") === "true",
   ACCESS_PHOTOS: true,
-
   ACCESS_HISTORY: true,
-
   COORDINATE_UPDATE: true
-
 };
+
+window.addEventListener(
+  "fieldtrial:settings-loaded",
+  event => {
+    const settings = event.detail || {};
+
+    window.FIELD_TRIAL_FEATURES.VISITS =
+      settings.trialVisits === true;
+
+    window.dispatchEvent(
+      new CustomEvent(
+        "fieldtrial:features-updated",
+        {
+          detail:
+            window.FIELD_TRIAL_FEATURES
+        }
+      )
+    );
+  }
+);
+``
